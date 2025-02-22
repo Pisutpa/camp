fe77f7aedd908dcfb71a172fa0a7aa46b37d926c
พิสุทธิ์ พาละพล <mkjuyna@gmail.com>
2025-02-23 00:55:48 +0700

    initial app
    navbar
    dropdownlist
    type

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+//rafce
+import { Button } from "@/components/ui/button"
+const homePage
+ = () => {
+  return (
+    <div>homePage
+<Button variant={'outline'}> Submit </Button>
+    </div>
+  )
+}
+export default homePage
---
+++ utils/links.ts
@@ -0,0 +1,11 @@
+type Navlinks = {
+    href:string;
+    label:string;
+}
+
+export const links :Navlinks[] = [
+    { href: '/', label: 'Home' },
+    { href: '/favorits', label: 'Faverits' },
+    { href: '/camp', label: 'Camp' }
+
+]
