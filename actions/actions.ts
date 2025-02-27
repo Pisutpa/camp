a88b58f8e01e9e44e00d352c426ef9c7cab2f9e2
พิสุทธิ์ พาละพล <mkjuyna@gmail.com>
2025-02-27 14:50:50 +0700

    input profile check client

--- actions/actions.ts
+++ actions/actions.ts
@@ -2,6 +2,18 @@
 "use server";
 
 import { profileSchema, validatedwithZod } from "@/utils/schemas";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import db from '@/utils/db'
+import { redirect } from "next/navigation";
+const getAuthUser = async()=>{
+    //code body
+const user = await currentUser()
+if(!user){
+    throw new Error('You must logged!!!')
+}
+if(!user.privateMetadata.hasProfile) redirect('/profile/create')
+return user
+}
 
 const renderError =(error:unknown):{message:string}=>{ 
 //code body 
@@ -16,13 +28,29 @@ export const createProfileAction = async (
      formData: FormData) => {
 
 try{
+    const user = await getAuthUser()
+
     const rawData = Object.fromEntries(formData)
     const validatedField = validatedwithZod(profileSchema,rawData)
-    console.log('validated',validatedField)
-    return { rawData: 'Create Profile Success!!' }
+   await db.profile.create({
+    data:{
+        clerkId:user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage:user.imageUrl?? '',
+        ...validatedField
+    }
+   })
+   const client = await clerkClient()
+   await client.users.updateUserMetadata(user.id,{
+    privateMetadata:{
+        hasProfile: true
+    }
+   })
+    //console.log('validated',validatedField)
+   // return { rawData: 'Create Profile Success!!' }
 }catch (error){
- console.log(error)
+ //console.log(error)
  return renderError(error)
 }
-    
+  redirect  ('/')
 };
